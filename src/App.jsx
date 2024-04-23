90e53ce1067f46b2ed5e7feabe37e4bb4460cf81
Angela Antova <antovaangela@gmail.com>
2024-04-23 20:59:07 +0200

    calculator

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import { Header } from "../src/components/Header";
+import { UserInput } from "../src/components/UserInput";
+import { Results } from "./components/Results";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue, // adding + to fix the issue with the number results
+      };
+    });
+  };
+
+  return (
+    <>
+      <Header />
+      <UserInput onChangeInput={handleChange} userInput={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results userInput={userInput} /> }
+    </>
+  )
+}
+
+export default App;
